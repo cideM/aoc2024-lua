6995144a7d178fbc5a1e7b85673617084ef53654
Florian Beeres <yuuki@protonmail.com>
2024-12-29 19:02:36 +0100

    update template

--- template.lua
+++ template.lua
@@ -2,35 +2,37 @@ INPUT = {}
 P1 = 0
 P2 = 0
 
--- GRID = {}
-
+-- ========= GRID =========
+-- G = {}
+--
 -- for line in io.lines() do
 -- 	local row = {}
 -- 	for ch in line:gmatch(".") do
 -- 		table.insert(row, ch)
 -- 	end
--- 	table.insert(GRID, row)
+-- 	table.insert(G, row)
+-- end
+--
+-- for y, row in ipairs(G) do
+-- 	for x, cell in ipairs(row) do
+-- 		print(cell)
+-- 	end
 -- end
 
--- local function skew_merge(h1, h2)
+-- ========= PRIORITY Q =========
+-- local function qmerge(h1, h2)
 -- 	if not h1 then return h2 end
 -- 	if not h2 then return h1 end
---
 -- 	if h1.value > h2.value then
 -- 		h1, h2 = h2, h1
 -- 	end
---
--- 	h1.right, h1.left = h1.left, skew_merge(h2, h1.right)
---
+-- 	h1.right, h1.left = h1.left, qmerge(h2, h1.right)
 -- 	return h1
 -- end
 --
--- local function skew_push(h, v) return skew_merge(h, { value = v, left = nil, right = nil }) end
+-- local function qpush(h, v) return qmerge(h, { value = v, left = nil, right = nil }) end
 --
--- local function skew_pop(h)
--- 	if not h then return nil, nil end
--- 	return h.value, skew_merge(h.left, h.right)
--- end
+-- local function qpop(h) return h and h.value, qmerge(h.left, h.right) or nil, nil end
 
 for line in io.lines() do
 	print(line)
